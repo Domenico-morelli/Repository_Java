ca2963e3abbc312daf599fc9bd62b5c241d3fc43
Domenico-morelli <domenicomorelliage@gmail.com>
2022-11-24 16:24:12 +0100

    Commit di calcolaPotenza Project

---
+++ code/calcolaPotenza/src/calcolaPotenza/it/corso/driver.java
@@ -0,0 +1,25 @@
+package calcolaPotenza.it.corso;
+
+import java.math.*;
+
+public class driver {
+
+	public static void main(String[] args) {
+	double i = 1;
+	System.out.println("\nFOR");
+		for (i = 1; i <= 16; i++) {
+			
+			System.out.println("2 elevato a " + i + " = " + Math.pow(2, i));
+		}
+		
+		i = 1;
+		System.out.println("\nWHILE");
+		while (i<16) {
+			
+			System.out.println("2 elevato a " + i + " = " + Math.pow(2, i));
+			i++;
+		}
+
+	}
+
+}
--- code/settimana/src/settimana/its/corso/Driver.java
+++ code/settimana/src/settimana/its/corso/Driver.java
@@ -6,34 +6,36 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Inseriscii il giorno della settimana: ");
-		int giorno = scanner.nextInt();
-		switch (giorno) {
-		case 0:
-			System.out.println("Il giorno della settimana è:" + Settimana.LUNEDI);
-			break;
-		case 1:
-			System.out.println("Il giorno della settimana è:" + Settimana.MARTEDI);
-			break;
-		case 2:
-			System.out.println("Il giorno della settimana è:" + Settimana.MERCOLEDI);
-			break;
-		case 3:
-			System.out.println("Il giorno della settimana è:" + Settimana.GIOVEDI);
-			break;
-		case 4:
-			System.out.println("Il giorno della settimana è:" + Settimana.VENERDI);
-			break;
-		case 5:
-			System.out.println("Il giorno della settimana è:" + Settimana.SABATO);
-			break;
-		case 6:
-			System.out.println("Il giorno della settimana è:" + Settimana.DOMENICA);
-			break;
-			
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + giorno);
-		}
-	}
+		System.out.println("Inserisci un numero da 1 a 7 e stamperò il relativo giorno della settimana");
+		int giorno;
 
+		do {
+			giorno = scanner.nextInt() - 1;
+			switch (giorno) {
+			case 0:
+				System.out.println("Il giorno della settimana è:" + Settimana.LUNEDI);
+				break;
+			case 1:
+				System.out.println("Il giorno della settimana è:" + Settimana.MARTEDI);
+				break;
+			case 2:
+				System.out.println("Il giorno della settimana è:" + Settimana.MERCOLEDI);
+				break;
+			case 3:
+				System.out.println("Il giorno della settimana è:" + Settimana.GIOVEDI);
+				break;
+			case 4:
+				System.out.println("Il giorno della settimana è:" + Settimana.VENERDI);
+				break;
+			case 5:
+				System.out.println("Il giorno della settimana è:" + Settimana.SABATO);
+				break;
+			case 6:
+				System.out.println("Il giorno della settimana è:" + Settimana.DOMENICA);
+				break;
+
+			}
+		} while (0 >= giorno || giorno <= 7);
+
+	}
 }
