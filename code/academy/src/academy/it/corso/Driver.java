570010b23b8bae1d288385bc57a8993cdfcfb789
Domenico-morelli <domenicomorelliage@gmail.com>
2022-11-24 12:55:06 +0100

    Commit per i salvataggi su Comandi git.txt

--- code/academy/src/academy/it/corso/Driver.java
+++ code/academy/src/academy/it/corso/Driver.java
@@ -2,8 +2,12 @@ package academy.it.corso;
 
 public class Driver {
 
+/*
+ * @Autor Domenico Morelli
+ * */
 	public static void main(String[] args) {
 		System.out.println("ciaone" + args[0]);
+		System.out.println("/n@Autor Domenico Morelli");
 	}
 
 }
