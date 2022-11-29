ca4af625f03842f2eb1f803f84118ccec31c207f
Domenico-morelli <domenico.morelli@studenti.unipg.it>
2022-11-29 09:38:47 +0100

    arrayprj

--- code/arrayprj/src/arrayprj/it/its/corso/Driver.java
+++ code/arrayprj/src/arrayprj/it/its/corso/Driver.java
@@ -1,21 +1,23 @@
 package arrayprj.it.its.corso;
 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		int valore, array[] = new int[10];
+		int array[] = new int[10];
 		Scanner scanner = new Scanner(System.in);
 
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Inserisci il valore " + (i+1));
-
 			array[i] = scanner.nextInt();
 		}
-		for (int i = 0; i < 10; i++) {
-			System.out.println("valore in posizione " + i + " dell'array " + +array[i]);
-
+		
+		Arrays.sort(array);
+		for(int arr: array) {
+			System.out.println(arr);
 		}
 
 	}
